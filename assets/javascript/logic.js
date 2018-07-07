a6c0180128bb28cbbe7c4b9a974078e71d4380d4
Eleni Pastra <hell-en@Elenis-MacBook-Pro.local>
2018-07-07 14:01:25 -0700

    latest code

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -20,25 +20,54 @@ var startDate = "";
 var monthlyRate = "";
 
 // Click Button changes what is stored in firebase
-$(document).ready(function){
-    $("#btn-primary").on("click", function(event) {
+$(document).ready(function() {
+    $(".btn-primary").on("click", function(event) {
     // Prevent the page from refreshing
-    event.preventDefault();
-       
+        event.preventDefault();
+        
+        // Get inputs
+        name = $("#add-name").val().trim();
+        role = $("#add-role").val().trim();
+        startDate = moment($("#add-date").val().trim(), "MM/DD/YYYY");
+        monthlyRate = $("#add-rate").val().trim();
 
-  // Get inputs
-  name = $("#add-name").val().trim();
-  role = $("#add-role").val().trim();
-  startDate = $("#add-date").val().trim();
-  monthlyRate = $("#add-rate").val().trim();
-})
+        var newEmp ={
+            name: name,
+            role: role,
+            startDate: startDate,
+            monthlyRate:monthlyRate
+        }
 
-database.ref().push({
-    name: name,
-    role: role,
-    startDate: startDate,
-    monthlyRate:monthlyRate
+        database.ref().push(newEmp);
+
+        console.log(newEmp.name);
+        console.log(newEmp.role);
+        console.log(newEmp.startDate);
+        console.log(newEmp.monthlyRate);
     
-  });
 
-}
+    })
+});  
+
+database.ref().on("child-added", function(childSnapshot){
+    console.log(childSnapshot.val());
+
+    // ?Store everythingin variables
+    var name =childSnapshot.val().name;
+    var role =childSnapshot.val().role;
+    var startDate =childSnapshot.val().startDate;
+    var monthlyRate =childSnapshot.val().monthlyRate;
+
+// missing code for moment.js to calculate time worked 
+
+
+// missing code to calculate the earnings since startDate
+    var empBilled = empMonths * empRate;
+    console.log(empBilled);
+
+// add varable to add a row that will then be appended with the new data we just collected
+    var newRow =$("<tr>").append(
+        $()
+    )
+
+})
