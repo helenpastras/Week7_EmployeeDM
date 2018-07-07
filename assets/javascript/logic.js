a7854cbec6140cc78d7dd90e4feeee57b7203123
Eleni Pastra <hell-en@Elenis-MacBook-Pro.local>
2018-07-07 12:44:12 -0700

    css and js updates

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDnOGRVfdTitXYzRQPaLjFKsWyq_Sd0gUQ",
+    authDomain: "schooldaywork.firebaseapp.com",
+    databaseURL: "https://schooldaywork.firebaseio.com",
+    projectId: "schooldaywork",
+    storageBucket: "schooldaywork.appspot.com",
+    messagingSenderId: "488651173054"
+  };
+  firebase.initializeApp(config);
+
 // Create a variable to reference the database
 var database = firebase.database();
 
@@ -9,14 +20,25 @@ var startDate = "";
 var monthlyRate = "";
 
 // Click Button changes what is stored in firebase
-$("#click-button").on("click", function(event) {
-  // Prevent the page from refreshing
-  event.preventDefault();
+$(document).ready(function){
+    $("#btn-primary").on("click", function(event) {
+    // Prevent the page from refreshing
+    event.preventDefault();
+       
 
   // Get inputs
-  name = $("#name-input").val().trim();
-  role = $("#role-input").val().trim();
-  startDate = $("#date-input").val().trim();
-  monthlyRate = $("#rate-input").val().trim();
-
+  name = $("#add-name").val().trim();
+  role = $("#add-role").val().trim();
+  startDate = $("#add-date").val().trim();
+  monthlyRate = $("#add-rate").val().trim();
 })
+
+database.ref().push({
+    name: name,
+    role: role,
+    startDate: startDate,
+    monthlyRate:monthlyRate
+    
+  });
+
+}
